5034accc9e9027d8feddfb87a48b919b398db1a4
FelAmore <114073117+FelAmore@users.noreply.github.com>
2023-01-11 09:46:44 +0700

    new version of main.c

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "myLib/myLib.h"
 
 void top() {
   printf("\n|Fel's Combination Lock FSM|");
 }
 
-// the visualization for the lock at state 0 
-void lockS0() { 
+// the visualization for the lock at state 00 
+void locks0() { 
     system("clear");
     // make the header
     top();
@@ -21,13 +22,13 @@ void lockS0() {
     printf(" |           |\n");
     printf(" |----|x|----|\n");
     printf(" |----|x|----|\n");
-    printf(" |----|x|____|\n");
+    printf(" |----|x|----|\n");
     printf(" |___________|\n");
     printf("     LOCKED\n");
 }
 
-// the visualization for the lock at state 1 
-void lockS1() {
+// the visualization for the lock at state 01 
+void locks1() {
     system("clear");
     // make the header
     top();
@@ -39,15 +40,15 @@ void lockS1() {
 
     //print the lock body
     printf(" |           |\n");
-    printf(" |----|3|----|\n");
+    printf(" |----|1|----|\n");
     printf(" |----|x|----|\n");
-    printf(" |----|x|____|\n");
+    printf(" |----|x|----|\n");
     printf(" |___________|\n");
     printf("     LOCKED\n");
 }
 
-// the visualization for the lock at state 2 
-void lockS2() {
+// the visualization for the lock at state 10 
+void locks2() {
     system("clear");
     // make the header
     top();
@@ -59,15 +60,15 @@ void lockS2() {
 
     //print the lock body
     printf(" |           |\n");
-    printf(" |----|3|----|\n");
-    printf(" |----|6|----|\n");
-    printf(" |----|x|____|\n");
+    printf(" |----|1|----|\n");
+    printf(" |----|2|----|\n");
+    printf(" |----|x|----|\n");
     printf(" |___________|\n");
     printf("     LOCKED\n");
 }
 
 // the visualization for the lock at the final state, the lock opened 
-void lockS3() {
+void locks3() {
     system("clear");
     // make the header
     top();
@@ -79,9 +80,9 @@ void lockS3() {
 
     //print the lock body
     printf(" |           |\n");
+    printf(" |----|1|----|\n");
+    printf(" |----|2|----|\n");
     printf(" |----|3|----|\n");
-    printf(" |----|6|----|\n");
-    printf(" |----|5|____|\n");
     printf(" |___________|\n");
     printf("    UNLOCKED\n");
 }
@@ -90,65 +91,76 @@ void lockS3() {
 
 int main(void) {
   // initialization
-  int p0 = 0;
-  int p1 = 0;
+  int s0 = 0;
+  int s1 = 0;
   int n0 = 0;
   int n1 = 0;
-  int input;
-  int output;
-  int inputD = 0;
+  int b0 = 0;
+  int b1 = 0;
+  int o1 = 0;
+  int input = 0;
   int display = 0;
 
   // fsm routine
   while(true) {
-    lockS0();
-    printf("\nEnter the first digit (0-9): ");
-    scanf("%d", &inputD);    
-    if (inputD == 3) {
-        input = 1;
-        display = 1;
-        lockS1();
+    if (s0 == 0 && s1 == 0) {
+      locks0();
+      printf("\n\nPresent State: %d %d", s0, s1);
+      printf("\nNext State: %d %d", n0, n1);
+      printf("\nInputs: %d %d", b0, b1);
+      printf("\nOutput: %d", o1);
     }
-    else {
-        input = 0;
-        display = 0;     
-        continue;
+    else if (s0 == 0 && s1 == 1) {
+      locks1();
+      printf("\n\nPresent State: %d %d", s0, s1);
+      printf("\nNext State: %d %d", n0, n1);
+      printf("\nInputs: %d %d", b0, b1);
+      printf("\nOutput: %d", o1);
     }
-    
-    printf("\nEnter the second digit (0-9): ");
-    scanf("%d", &inputD);
-    if (inputD == 6) {
-        input = 1;
-        display = 2;
-        lockS2();
+    else if (s0 == 1 && s1 == 0) {
+      locks2();
+      printf("\n\nPresent State: %d %d", s0, s1);
+      printf("\nNext State: %d %d", n0, n1);
+      printf("\nInputs: %d %d", b0, b1);
+      printf("\nOutput: %d", o1);
     }
-    else {
-        input = 0;
-        display = 0;
-        continue;
-    }
-
-    printf("\nEnter the third digit (0-9): ");
-    scanf("%d", &inputD);
-    if (inputD == 5) {
-        input = 1;
-        display = 3;
-        lockS3();       
-    }
-    else {
-        input = 0;
-        display = 0;
-        continue;
+    else if (s0 == 1 && s1 == 1) {
+      locks3();
+      printf("\n\nPresent State: %d %d", s0, s1);
+      printf("\nNext State: %d %d", n0, n1);
+      printf("\nInputs: %d %d", b0, b1);
+      printf("\nOutput: %d", o1);
     }
     
-    printf("\nEnter any digit to reset (0-9): ");
-    scanf("%d", &inputD);
-    input = 0;
-    display = 0;
+    // take input from user
+    printf("\n\nEnter input here (0-3): ");
+    // save the input for user into the input variable
+    scanf("%d", &input);
+    
+    // transforming the input from user into inputs accepted by the machine
+    if (input == 1) {
+      b0 = 0;
+      b1 = 0;
+    }
+    else if (input == 2) {
+      b0 = 0;
+      b1 = 1;
+    }
+    else if (input == 3) {
+      b0 = 1;
+      b1 = 0;
+    }
+    else if (input == 0) {
+      b0 = 1;
+      b1 = 1;
+    }
 
-    calcNextState(p0, p1, input, n0, n1);
-    calcOutput(p0, p1, output);
-}
-  
+    // call function to calculate next state and output
+    calcNextState(s0, s1, b0, b1, &n0, &n1);
+    // setting the present state
+    s0 = n0;
+    s1 = n1;
+    calcOutput(s0, s1, &o1);
+  } 
 return 0;
 }
